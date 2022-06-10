35a74e38d12143bc4b9f6283a7170d4cfe865a1b
tygrski <77369211+tygrski@users.noreply.github.com>
2022-06-09 21:13:22 -0500

    last part

--- main.js
+++ main.js
@@ -389,10 +389,41 @@
 // btn.style.background = 'red';
 
 //  EVENTS
-const btn = document.querySelector('.btn');
+// const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', (e) =>{
+// btn.addEventListener('mouseout', (e) =>{
+//     e.preventDefault();
+//  document.querySelector('#my-form').style.background = '#ccc';
+//  document.querySelector('body').classList.add('bg-dark');
+//  document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>'
+// });
+
+// DOM to vaiables
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msgm = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
     e.preventDefault();
- console.log(e.target);
-});
 
+   if(nameInput.value === '' || emailInput.value === '') {
+       msgm.classList.add('error');
+      msgm.innerHTML = 'enter info';
+
+
+      setTimeout(() => msgm.remove(), 3000)
+   }else {
+      const li = document.createElement('li');
+      li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+//   append the li to the UL
+    userList.appendChild(li);
+
+    // clear fields
+    nameInput.value = '';
+    emailInput.value = '';
+    }
+};
